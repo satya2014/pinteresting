4421da4d265d69cfc2e6e7971b2b6639edbe154c
Satyajeet <satyajeet@money-wizards.com>
2014-10-22 15:26:32 +0530

    Didn't Configure Amazon Web Services hence image will not be shown on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,14 @@ Pinteresting::Application.configure do
   # Required for Heroku
   # Remember to set this for actual host name
   config.action_mailer.default_url_options = { :host => 'http://satya-pinteresting.herokuapp.com/' }
+
+  # Sets paperclip to upload images to Heroku Amazon S3 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+   }
+  }
 end
