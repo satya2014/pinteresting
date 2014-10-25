f280344d61cae91c04f9556101ff6a4c61172c6a
Satyajeet <satyajeet@money-wizards.com>
2014-10-25 15:01:51 +0530

    Created the validation of Image Uploading

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,7 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
      has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://4vector.com/i/free-vector-tango-image-missing_098855_tango_image_missing.png"
+
+     validates :image, presence: true
+     validates :description, presence: true
 end
